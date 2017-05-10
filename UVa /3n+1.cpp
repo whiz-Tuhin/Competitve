284715be71d0b2ed6664901028b2095390aee64f
Tuhin Khare <tuhinkhare3@gmail.com>
2017-05-10 16:31:08 +0530

    Added Collatz conjecture problem

---
+++ UVa /3n+1.cpp
@@ -0,0 +1,56 @@
+//Problem is based on the collatz conjecture
+// where you have a cyclic repition when every number is plugged into the function
+/*
+f(x) -> {
+              n/2     if n is even
+
+              3*n+1   if n is odd
+
+        }
+every number converges to 1 when plugged into the above recursive function.
+
+below is a brute force implementation, but a better solution would be to use caching 
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int cycle_len = 0;
+
+int collatz_cycle(int i){
+    int count = 0;
+    while(1){
+        count++;
+        if(i == 1){
+            break;
+        }
+        else if(i%2 == 0){
+              i = i/2;
+
+        }
+        else if(i%2 != 0 && i!=1){
+              i = 3*i+1;
+        }
+    }
+    return count;
+}
+
+int main(){
+
+    int a,b;
+    cin>>a>>b;
+
+    int max = -1;
+    for(int i = a;i <= b;i++){
+            int c = collatz_cycle(i);
+            if(c > max){
+                max = c;
+            }
+            cycle_len = 0;
+    }
+    cout<<a<<" "<<b<<" "<<max<<endl;
+    return 0;
+}
